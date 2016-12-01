15197bec477d8b3b93e5a2de239b8f27e260fc90
Angel Beltran <angel.beltran@tcnbroadcasting.com>
2016-11-30 19:09:15 -0700

    whoops

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@ echo alias kubeexec=`pwd`/kubeexec.sh >> ~/.bashrc
 echo alias kubedestroy=`pwd`/kubedestroy.sh >> ~/.bashrc
 echo alias kubelog=`pwd`/kubelog.sh >> ~/.bashrc
 
-alias kubeexec=`pwd`/kubeexec.sh >> ~/.bashrc
-alias kubedestroy=`pwd`/kubedestroy.sh >> ~/.bashrc
-alias kubelog=`pwd`/kubelog.sh >> ~/.bashrc
+alias kubeexec=`pwd`/kubeexec.sh
+alias kubedestroy=`pwd`/kubedestroy.sh
+alias kubelog=`pwd`/kubelog.sh
